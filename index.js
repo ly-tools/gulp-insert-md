81a23a885711e8547e16973c468866172cd4f680
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2016-01-04 19:26:44 +0300

    :smile:

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const _ = require('lodash');
 const through = require('through2');
 const DEFAULT_OPTS = {};
 const insert = require('./lib/index');
-const logger = require('./lib/logger');
+const PluginError = require('gulp-util').PluginError;
 
 module.exports = opts => {
   opts = _.defaults(opts || {}, DEFAULT_OPTS);
   return through.obj((file, encoding, callback) => {
+    if (file.isNull()) return callback(null, file);
+    if (file.isStream()) return callback(new PluginError('gulp-insert-md', `Stream is not supported`));
     insert(file, opts)
-      .catch(e => logger.error(`${e.message}`))
-      .then(() => callback(null, file));
+      .then(() => callback(null, file))
+      .catch(e => callback(new PluginError('gulp-insert-md', e)));
   });
 };
